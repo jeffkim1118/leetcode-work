52e3c34ae26debcb6e98255ed6ba091bbde5bc78
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-10-13 13:07:51 -0400

    Time: 117 ms (53.32%), Space: 44 MB (47.24%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,28 +3,35 @@
  * @return {number}
  */
 var reverse = function(x) {
-    // if(x < 0){
-    //     return parseInt("-" + x.toString().split('').reverse().join(''));
-    // }
-    // return parseInt(x.toString().split('').reverse().join(''));
-    
-    
-    
-    let current = 0, negative = false;
-    if(x < 0) negative = true;
-    
-    // Make the number positive 
-    x = Math.abs(x)
-    
-    while( x > 0) {
-        current = current * 10 + x % 10;
-        x = Math.floor(x / 10)
+    if(x < 0){
+        let result = parseInt("-" + x.toString().split('').reverse().join(''));
+        if(result < (Math.pow(2, 31) * -1) || result > Math.pow(2, 31) - 1){
+            return 0;
+        }return result
+    }else{
+        let result = parseInt(x.toString().split('').reverse().join(''))
+        if(result < (Math.pow(2, 31) * -1) || result > Math.pow(2, 31) - 1){
+            return 0;
+        }return result
     }
     
-    // Make sure the size is 32 bit 
-    if(current > Math.pow(2, 31) - 1 || current < Math.pow(-2, 31)) return 0;
     
-    return negative ? current * -1 : current;
+    
+//     let current = 0, negative = false;
+//     if(x < 0) negative = true;
+    
+//     // Make the number positive 
+//     x = Math.abs(x)
+    
+//     while( x > 0) {
+//         current = current * 10 + x % 10;
+//         x = Math.floor(x / 10)
+//     }
+    
+//     // Make sure the size is 32 bit 
+//     if(current > Math.pow(2, 31) - 1 || current < Math.pow(-2, 31)) return 0;
+    
+//     return negative ? current * -1 : current;
     
     
 };
